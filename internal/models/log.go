f75f81e6a46ce38a4d3caa5c0e2a0d5f28a97c46
Danya97i <dan.deg97@vk.com>
2024-10-28 23:36:53 +0300

    add description

--- internal/models/log.go
+++ internal/models/log.go
@@ -1,14 +1,16 @@
 package models
 
+// Action is the type of action
 type Action string
 
 const (
-	ActionCreate Action = "create"
-	ActionGet    Action = "get"
-	ActionUpdate Action = "update"
-	ActionDelete Action = "delete"
+	ActionCreate Action = "create" // ActionCreate - add user
+	ActionGet    Action = "get"    // ActionGet - get user
+	ActionUpdate Action = "update" // ActionUpdate - update user
+	ActionDelete Action = "delete" // ActionDelete - delete user
 )
 
+// LogInfo is the log info
 type LogInfo struct {
 	UserID int64
 	Action Action
